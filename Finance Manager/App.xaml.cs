c7eb68654f6f27ae011f8be48617fa473efe31f8
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-03-24 19:37:19 +0200

    Added Dependency Injection and DbContext.

--- Finance Manager/App.xaml.cs
+++ Finance Manager/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Finance_Manager.DataBase;
+using Microsoft.Extensions.DependencyInjection;
+using Finance_Manager.ViewModels;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance_Manager
 {
@@ -9,6 +11,33 @@ namespace Finance_Manager
     /// </summary>
     public partial class App : Application
     {
-    }
+        public static IServiceProvider ServiceProvider { get; private set; }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = "Server=localhost;Database=FinanceManagerDB;Integrated Security=True;";
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddSingleton<LoginViewModel>();
+            services.AddSingleton<TransactionsViewModel>();
+            services.AddSingleton<AnalyticsViewModel>();
+            services.AddSingleton<SavingsViewModel>();
+            services.AddSingleton<PlanningViewModel>();
+            services.AddSingleton<SettingsViewModel>();
+
+            services.AddSingleton<MainWindow>();
+        }      
+    }
 }
