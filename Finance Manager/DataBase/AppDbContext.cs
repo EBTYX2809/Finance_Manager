6c3d893c6652ac7ee4bb132bf21c131acc753021
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-03-26 05:36:24 +0200

    Did first migration with EF.

--- Finance Manager/DataBase/AppDbContext.cs
+++ Finance Manager/DataBase/AppDbContext.cs
@@ -1,18 +1,32 @@
-﻿using Finance_Manager.Models;
+﻿using Finance_Manager.Configurations;
+using Finance_Manager.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Finance_Manager.DataBase;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<User> Users { get; set; }
     public DbSet<Transaction> Transaction { get; set; }
     public DbSet<Saving> Savings { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //string connectionString = "server=localhost;database=TestDB;user=ebtyx;password=atm28foot";
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+        modelBuilder.ApplyConfiguration(new SavingConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
 
-        //options.UseSqlServer(connectionString, ServerVersion.AutoDetect(connectionString));
+        base.OnModelCreating(modelBuilder);
     }
+
+    /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=FinanceManagerDB;Integrated Security=True;");
+        }
+    }*/
 }
---
+++ Finance Manager/DataBase/AppDbContextFactory .cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Finance_Manager.DataBase;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var Config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+        string? connectionString = Config.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
