98ed534e9311389bac455358aefe12aadba53158
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-04-10 20:44:39 +0300

    Added Db fixture for tests.

---
+++ Finance_Manager_Backend_Tests/DataBase/TestDbContextFixture.cs
@@ -0,0 +1,29 @@
+﻿using Finance_Manager_Backend.DataBase;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Finance_Manager_Tests.DataBase
+{
+    public class TestDbContextFixture : IDisposable
+    {
+        public AppDbContext dbContext;
+        public TestDbContextFixture() 
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Test.json")
+            .Build();            
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer(config.GetConnectionString("DefaultConnection"))
+                .Options;
+
+            dbContext = new AppDbContext(options);
+        }
+
+        public void Dispose() 
+        {
+            dbContext?.Dispose();
+        }
+    }
+}
