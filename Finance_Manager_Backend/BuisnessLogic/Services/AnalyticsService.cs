02972f59d1dec88f7dfb89d6193dc3b7bfd09cb6
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-04-13 15:54:28 +0300

    Created AnalyticsService.

---
+++ Finance_Manager_Backend/BuisnessLogic/Services/AnalyticsService.cs
@@ -0,0 +1,65 @@
+﻿using Finance_Manager_Backend.BuisnessLogic.Models;
+using Finance_Manager_Backend.DataBase;
+using Finance_Manager_Backend.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance_Manager_Backend.BuisnessLogic.Services;
+
+public class AnalyticsService
+{
+    private AppDbContext _appDbContext;    
+    public AnalyticsService(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;        
+    }
+
+    public async Task<Dictionary<Category, float>> GetAlalyticFromDate(int userId, DateTime minDate, DateTime maxDate)
+    {
+        var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+        if (user == null) throw new UserNotFoundException(userId.ToString());
+        
+        var spendsByCategory = await _appDbContext.Transactions                
+            .Where(s => s.UserId == userId 
+                     && !s.Category.IsIncome 
+                     && s.Date > minDate 
+                     && s.Date < maxDate)                      
+            .GroupBy(s => s.Category)
+            .ToDictionaryAsync(g => g.Key, g => g.Sum(x => x.Price));
+
+        decimal generalSpends = spendsByCategory.Values.Sum();
+        Dictionary<Category, float> percentsByCategory = new();
+
+        foreach(var s in spendsByCategory)
+        {
+            percentsByCategory.Add(s.Key, (float)(s.Value / generalSpends * 100));
+        }
+
+        return percentsByCategory;
+    }
+
+    public async Task<Dictionary<Category, float>> GetInnerAlalyticFromDate(int userId, Category parentCategory, DateTime minDate, DateTime maxDate)
+    {
+        var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+        if (user == null) throw new UserNotFoundException(userId.ToString());
+
+        var spendsByCategory = await _appDbContext.Transactions
+            .Where(s => s.UserId == userId
+                     && s.Category == parentCategory
+                     && s.Date > minDate
+                     && s.Date < maxDate)
+            .GroupBy(s => s.InnerCategory ?? new Category()) // ?? new Category() suppress null alerts.
+            .ToDictionaryAsync(g => g.Key, g => g.Sum(x => x.Price));
+
+        decimal generalSpends = spendsByCategory.Values.Sum();
+        Dictionary<Category, float> percentsByCategory = new();
+
+        foreach (var s in spendsByCategory)
+        {
+            percentsByCategory.Add(s.Key, (float)(s.Value / generalSpends * 100));
+        }
+
+        return percentsByCategory;
+    }
+}
