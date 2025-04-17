70d02dcb98734b68ca6bb3362dcb6530430caa2e
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-04-17 17:49:34 +0300

    Added jwt token generator.

---
+++ Finance_Manager_Backend/BusinessLogic/Services/AuthServices/JwtTokenGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Finance_Manager_Backend.BusinessLogic.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Finance_Manager_Backend.BusinessLogic.Services.AuthServices;
+
+public class JwtTokenGenerator
+{
+    private readonly IConfiguration _config;
+    public JwtTokenGenerator(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var jwtSettings = _config.GetSection("JwtSettings");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: jwtSettings["Issuer"],
+            audience: jwtSettings["Audience"],
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiresInMinutes"])),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
