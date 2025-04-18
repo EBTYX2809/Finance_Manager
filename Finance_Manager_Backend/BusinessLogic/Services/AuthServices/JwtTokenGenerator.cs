9ff825429ff715cae694acf7e80c743be832d048
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-04-18 22:40:28 +0300

    Added claims to GwtTokenGenerator for generating more unique tokens.

--- Finance_Manager_Backend/BusinessLogic/Services/AuthServices/JwtTokenGenerator.cs
+++ Finance_Manager_Backend/BusinessLogic/Services/AuthServices/JwtTokenGenerator.cs
@@ -14,16 +14,23 @@ public class JwtTokenGenerator
         _config = config;
     }
 
-    public string GenerateToken()
+    public string GenerateToken(User user)
     {
         var jwtSettings = _config.GetSection("JwtSettings");
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+        };
+
         var token = new JwtSecurityToken(
             issuer: jwtSettings["Issuer"],
             audience: jwtSettings["Audience"],
             expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiresInMinutes"])),
+            claims: claims,
             signingCredentials: creds
         );
 
