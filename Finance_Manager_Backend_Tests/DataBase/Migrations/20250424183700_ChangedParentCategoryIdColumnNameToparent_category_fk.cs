41f06667a53ce904778d1a60d585eb9686ed5b9e
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-04-25 19:35:01 +0300

    Updated test data base with rename column parent_category_FK.

---
+++ Finance_Manager_Backend_Tests/DataBase/Migrations/20250424183700_ChangedParentCategoryIdColumnNameToparent_category_fk.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finance_Manager_Backend_Tests.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedParentCategoryIdColumnNameToparent_category_fk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /*migrationBuilder.DropForeignKey(
+                name: "FK_categories_categories_parent_category_fk",
+                table: "categories");*/
+
+            migrationBuilder.RenameColumn(
+                name: "ParentCategoryId",
+                table: "categories",
+                newName: "parent_category_fk");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_categories_ParentCategoryId",
+                table: "categories",
+                newName: "IX_categories_parent_category_fk");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_categories_categories_parent_category_fk",
+                table: "categories",
+                column: "parent_category_fk",
+                principalTable: "categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_categories_categories_parent_category_fk",
+                table: "categories");
+
+            migrationBuilder.RenameColumn(
+                name: "parent_category_fk",
+                table: "categories",
+                newName: "ParentCategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_categories_parent_category_fk",
+                table: "categories",
+                newName: "IX_categories_ParentCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_categories_categories_ParentCategoryId",
+                table: "categories",
+                column: "ParentCategoryId",
+                principalTable: "categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
