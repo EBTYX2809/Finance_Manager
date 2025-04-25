642b3832e7f3ca8cf2068663fe34ce57fd90d2b0
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-04-25 19:38:36 +0300

    Added auto mapper and FluentAssertions for proper objects assertion in TransactionsServiceTests.

--- Finance_Manager_Backend_Tests/ServicesTests/TransactionsServiceTests.cs
+++ Finance_Manager_Backend_Tests/ServicesTests/TransactionsServiceTests.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Logging;
 using Finance_Manager_Backend.BusinessLogic.Models;
 using Microsoft.EntityFrameworkCore;
 using Xunit.Abstractions;
+using AutoMapper;
+using Finance_Manager_Tests.ServicesTests;
+using Finance_Manager_Backend.BusinessLogic.Models.DTOs;
+using FluentAssertions;
 
 namespace Finance_Manager_Backend_Tests.ServicesTests;
 
@@ -19,16 +23,21 @@ public class TransactionsServiceTests
     private TransactionsService _transactionsService;
     private readonly ITestOutputHelper _output;
     private readonly UsersService _usersService;
+    private readonly CategoriesService _categoriesService;
+    private readonly IMapper _mapper;
 
     public TransactionsServiceTests(TestDbContextFixture fixture, ITestOutputHelper output)
     {
         _appDbContext = fixture.dbContext;
+        _mapper = AutoMapperFotTests.GetMapper();
         _mockLoggerTS = new Mock<ILogger<TransactionsService>>();
         _mockLoggerTT = new Mock<ILogger<DbTransactionTemplate>>();
         _transactionTemplate = new DbTransactionTemplate(_appDbContext, _mockLoggerTT.Object);
+        _categoriesService = new CategoriesService(_appDbContext, _mapper);
         _usersService = new UsersService(_appDbContext);
-        _transactionsService = new TransactionsService(_appDbContext, _transactionTemplate, _mockLoggerTS.Object, _usersService);
-        _output = output;
+        _transactionsService = new TransactionsService(_appDbContext, _transactionTemplate, 
+            _mockLoggerTS.Object, _usersService, _mapper, _categoriesService);
+        _output = output;        
     }
 
     [Fact]
@@ -38,16 +47,15 @@ public class TransactionsServiceTests
         var user = await _appDbContext.Users.FirstOrDefaultAsync();
         var category = await _appDbContext.Categories.FirstOrDefaultAsync();
         var transaction = new Transaction("New transaction form test", 100.89m, DateTime.Now, category, user);
+        var transactionDTO = _mapper.Map<TransactionDTO>(transaction);
 
         // Act
-        await _transactionsService.CreateTransactionAsync(transaction);
-        var createdTransaction = await _appDbContext.Transactions
-            .Include(t => t.Category)   
-            .Include(t => t.User)
-            .FirstOrDefaultAsync(c => c.Id == transaction.Id);
+        await _transactionsService.CreateTransactionAsync(transactionDTO);
+        var createdTransaction = await _appDbContext.Transactions.FirstOrDefaultAsync(c => c.Id == transactionDTO.Id);
+        var createdTransactionDTO = _mapper.Map<TransactionDTO>(createdTransaction);
 
         // Assert
-        Assert.Equal(transaction, createdTransaction);
+        createdTransactionDTO.Should().BeEquivalentTo(transactionDTO);
     }
 
     [Fact]
@@ -78,8 +86,9 @@ public class TransactionsServiceTests
 
         // Act
         oldTransaction.Name = "New name";
+        var oldTransactionDTO = _mapper.Map<TransactionDTO>(oldTransaction);        
 
-        await _transactionsService.UpdateTransactionAsync(oldTransaction);
+        await _transactionsService.UpdateTransactionAsync(oldTransactionDTO);
 
         var newTransaction = await _appDbContext.Transactions
             .Include(t => t.Category)
@@ -87,7 +96,7 @@ public class TransactionsServiceTests
             .FirstOrDefaultAsync(t => t.Id == oldTransaction.Id);
 
         // Assert
-        Assert.Equal(oldTransaction, newTransaction);
+        newTransaction.Should().BeEquivalentTo(oldTransaction);
     }
 
     [Fact]
