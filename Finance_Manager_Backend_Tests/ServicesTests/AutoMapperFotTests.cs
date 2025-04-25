e5521236230e6ab2f2a94c53b1b71b4ba139f028
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-04-25 19:36:25 +0300

    Created auto mapper for tests.

---
+++ Finance_Manager_Backend_Tests/ServicesTests/AutoMapperFotTests.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Finance_Manager_Backend.BusinessLogic.Models;
+
+namespace Finance_Manager_Tests.ServicesTests;
+
+public static class AutoMapperFotTests
+{
+    public static IMapper GetMapper()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<AutoMapperProfile>();
+        });       
+
+        return config.CreateMapper();
+    }
+}
